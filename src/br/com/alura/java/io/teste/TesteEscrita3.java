0ffccb8ae29a376d5481ea4a8d097d39a2eaf6b0
Cassio Murilo <cmurilo.san@gmail.com>
2020-06-15 12:11:52 -0300

    Aula 03 - PrintWriter e PrintStream

---
+++ src/br/com/alura/java/io/teste/TesteEscrita3.java
@@ -0,0 +1,22 @@
+package br.com.alura.java.io.teste;
+
+import java.io.*;
+
+public class TesteEscrita3 {
+
+    public static void main(String[] args) throws IOException {
+
+        //Fluxo de Entrada com Arquivo
+//        OutputStream fos = new FileOutputStream("lorem2.txt");
+//        Writer osw = new OutputStreamWriter(fos);
+//        BufferedWriter bw = new BufferedWriter(osw);
+
+//        BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+//        PrintStream ps = new PrintStream("lorem2.txt");
+        PrintWriter ps = new PrintWriter("lorem2.txt");
+        ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+        ps.println();
+        ps.println("teste usando a Classe PrintWriter");
+        ps.close();
+    }
+}
