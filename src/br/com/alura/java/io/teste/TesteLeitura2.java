f5df30eebcf21ac73de10642a0c06531bcde1c24
Cassio Murilo <cmurilo.san@gmail.com>
2020-06-15 18:18:05 -0300

    Aula 04 - Formatação de Valores

--- src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -1,9 +1,6 @@
 package br.com.alura.java.io.teste;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -14,19 +11,21 @@ public class TesteLeitura2 {
         Scanner scanner = new Scanner(new File("contas.csv"));
         while (scanner.hasNextLine()) {
             String linha = scanner.nextLine();
-            System.out.println(linha);
+//            System.out.println(linha);
 
             Scanner linhaScanner = new Scanner(linha);
             linhaScanner.useLocale(Locale.US);
             linhaScanner.useDelimiter(",");
 
-            String valor1 = linhaScanner.next();
-            int valor2 = linhaScanner.nextInt();
-            int valor3 = linhaScanner.nextInt();
-            String valor4 = linhaScanner.next();
-            double valor5 = linhaScanner.nextDouble();
+            String tipoConta = linhaScanner.next();
+            int agencia = linhaScanner.nextInt();
+            int numero = linhaScanner.nextInt();
+            String titular = linhaScanner.next();
+            double saldo = linhaScanner.nextDouble();
 
-            System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+            System.out.format(new Locale("pt", "BR"),
+                    "%s - %04d-%08d, %20s: %08.2f %n",
+                    tipoConta, agencia, numero, titular, saldo);
 
             linhaScanner.close();
 
