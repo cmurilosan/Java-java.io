ab3cccd019531fef1064295b10db622c909ef09b
Cassio Murilo <cmurilo.san@gmail.com>
2020-06-14 23:28:16 -0300

    Aula 02 - Flexibilidade no I/O

---
+++ src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -0,0 +1,32 @@
+package br.com.alura.java.io.teste;
+
+import java.io.*;
+import java.net.Socket;
+
+public class TesteCopiarArquivo {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket s = new Socket();
+
+        InputStream fis = s.getInputStream();//Rede   //System.in; (Teclado)    //new FileInputStream("lorem.txt");(arquivo)
+        Reader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        OutputStream fos = s.getOutputStream();//(Rede) //System.out;(Terminal) //new FileOutputStream("lorem2.txt");(arquivo)
+        Writer osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        String linha = br.readLine();
+
+        while (linha != null && !linha.isEmpty()) {
+            bw.write(linha);
+            bw.newLine();
+            bw.flush();
+            linha = br.readLine();
+        }
+
+        br.close();
+        bw.close();
+    }
+}
