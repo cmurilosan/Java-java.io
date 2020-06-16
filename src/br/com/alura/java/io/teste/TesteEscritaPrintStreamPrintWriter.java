acd5f7c7f5ede39cc1b45ae0cdef3ce68d3d6bff
Cassio Murilo <cmurilo.san@gmail.com>
2020-06-16 12:36:12 -0300

    Aula 05 - Encoding com java.io

--- src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
+++ src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
@@ -13,7 +13,7 @@ public class TesteEscritaPrintStreamPrintWriter {
 
 //        BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
 //        PrintStream ps = new PrintStream("lorem2.txt");
-        PrintWriter ps = new PrintWriter("lorem2.txt");
+        PrintWriter ps = new PrintWriter("lorem2.txt", "UTF-8");
         ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
         ps.println();
         ps.println("teste usando a Classe PrintWriter");
--- src/br/com/alura/java/io/teste/TesteUnicodeEEncoding.java
+++ src/br/com/alura/java/io/teste/TesteUnicodeEEncoding.java
@@ -1,10 +1,34 @@
 package br.com.alura.java.io.teste;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class TesteUnicodeEEncoding {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {    //para nos assegurarmos de que o charset existe
 
-        String s = "C";
-        System.out.println(s.codePointAt(0));
+        String s = "ç";
+        System.out.println(s.codePointAt(0));   //descobrirmos qual é o codepoint, que é o número associado na tabela de unicodes
+
+        Charset charset = Charset.defaultCharset();   //descobrir qual é o encoding padrão.
+        System.out.println(charset.displayName());    // UTF-8
+
+        byte[] bytes = s.getBytes("UTF-8");
+        System.out.print(bytes.length + ", UTF-8, ");
+        String sNovo = new String(bytes, "UTF-8" );
+        System.out.println(sNovo);
+
+        bytes = s.getBytes("UTF-16");
+        System.out.print(bytes.length + ", UTF-16, ");
+        sNovo = new String(bytes, "UTF-16");
+        System.out.println(sNovo);
+
+        bytes = s.getBytes(StandardCharsets.US_ASCII);
+        System.out.print(bytes.length + ", US-ASCII, ");
+        sNovo = new String(bytes, "US-ASCII");
+        System.out.println(sNovo);
+
+        System.out.println("O ASCII continua apresentando erro, pois a ç não está presenta na tabela padrão.");
     }
 }
